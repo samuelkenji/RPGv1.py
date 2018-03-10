354a80745da02156c5cba8cea6132a9a03cdd140
samuelkenji <sa.kgs10@gmail.com>
2018-03-10 12:43:28 -0300

    rpg

--- RPGv1.py
+++ RPGv1.py
@@ -14,3 +14,10 @@ if r_encounter<35:
             print('Você deu 10 de dano')
         else:
             print('Ataque crítico! 20 de dano')
+    elif decisao='Defender' or decisao='defender':
+        defesa=int(randint(1,20))
+        if defesa<10:
+            hp1=hp0-5
+            print('Sua defesa falhou')
+        else:
+            print('Você bloqueou o ataque com sucesso!')
