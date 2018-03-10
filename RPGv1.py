4056555cb092c703bfe6e2dc441ad0b970dddb88
samuelkenji <sa.kgs10@gmail.com>
2018-03-10 12:34:46 -0300

    rpg

--- RPGv1.py
+++ RPGv1.py
@@ -4,11 +4,13 @@ hp_mob1=50
 r_encounter=int(randint(0,100))
 if r_encounter<35:
     print('Um inimigo se aproxima')
-    combate=int(randint(1,20))
-    if combate<10:
-        hp1=hp0-10
-        print('Você perdeu 10 de vida')
-    elif combate>10 and combate<20:
-        print('Você deu 10 de dano')
-    else:
-        print('Ataque crítico! 20 de dano')
+    decisao=input('Atacar   Defender    Fugir')
+    if decisao='Atacar' or decisao='atacar':
+        combate=int(randint(1,20))
+        if combate<10:
+            hp1=hp0-10
+            print('Você perdeu 10 de vida')
+        elif combate>10 and combate<20:
+            print('Você deu 10 de dano')
+        else:
+            print('Ataque crítico! 20 de dano')
