8581aeafd5bf519f1c946188ee7a511398e988c5
samuelkenji <sa.kgs10@gmail.com>
2018-03-19 07:11:45 -0300

    rpg

--- RPGv1.py
+++ RPGv1.py
@@ -13,63 +13,65 @@ while hp0 > 0:
             ataque_mob=randint(1,10)
             if mob_encounter<35:
                 hp_mob1 = 50
-                print('Um inimigo se aproxima')
-                while hp_mob1 > 0:
-                    print('Mob: {}  Você: {}'.format(hp_mob1,hp0))
-                    decisao = int(input('1-Atacar   2-Defender    3-Fugir     4-Item: '))
-                    if decisao == 1:
-                        combate = int(randint(1, 20))
-                        if combate < 10 and combate>4:
-                            hp_mob1 = hp_mob1 - 5
-                            print('Você deu 5 de dano')
-                            print('O inimigo ataca de volta te dando {} de dano.'.format(ataque_mob))
-                            hp0=hp0-ataque_mob
-                        elif combate < 3:
-                            print('Errou!')
-                            print('O inimigo ataca de volta te dando {} de dano'.format(ataque_mob))
-                            hp0=hp0-ataque_mob
-                        elif combate > 10 and combate < 20:
-                            hp_mob1 = hp_mob1 - 10
-                            print('Você deu 10 de dano')
-                            print('O inimigo ataca de volta te dando {} de dano'.format(ataque_mob))
-                            hp0=hp0-ataque_mob
+            elif mob_encounter>75:
+                hp_mob1=60
+            print('Um inimigo se aproxima')
+            while hp_mob1 > 0:
+                print('Mob: {}  Você: {}'.format(hp_mob1,hp0))
+                decisao = int(input('1-Atacar   2-Defender    3-Fugir     4-Item: '))
+                if decisao == 1:
+                    combate = int(randint(1, 20))
+                    if combate < 10 and combate>4:
+                        hp_mob1 = hp_mob1 - 5
+                        print('Você deu 5 de dano')
+                        print('O inimigo ataca de volta te dando {} de dano.'.format(ataque_mob))
+                        hp0=hp0-ataque_mob
+                    elif combate < 3:
+                        print('Errou!')
+                        print('O inimigo ataca de volta te dando {} de dano'.format(ataque_mob))
+                        hp0=hp0-ataque_mob
+                    elif combate > 10 and combate < 20:
+                        hp_mob1 = hp_mob1 - 10
+                        print('Você deu 10 de dano')
+                        print('O inimigo ataca de volta te dando {} de dano'.format(ataque_mob))
+                        hp0=hp0-ataque_mob
+                    else:
+                        hp_mob1 = hp_mob1 - 20
+                        print('Ataque crítico! 20 de dano')
+                        print('O inimigo ataca de volta te dando {} de dano'.format(ataque_mob))
+                        hp0=hp0-ataque_mob
+                elif decisao == 2:
+                    defesa = int(randint(1, 20))
+                    if defesa < 5:
+                        hp0 = hp0 - 5
+                        print('Sua defesa falhou')
+                        print('Você levou 5 de dano')
+                    else:
+                        print('Você bloqueou o ataque com sucesso!')
+                elif decisao == 3:
+                    fuga = int(randint(1, 20))
+                    if fuga < 4:
+                        print('Parece que algúem não quer que você fuja')
+                    else:
+                        print('Você escapou com sucesso')
+                        fugiu=fugiu+1
+                        if fugiu==1:
+                            break
+                elif decisao == 4:
+                    print('Qual item você deseja usar?')
+                    inv=int(input('Inventário:\n    1-Poção - {}    2-Voltar'.format(pot)))
+                    if inv==1:
+                        if pot>=1:
+                            hp0=hp0+25
+                            print('Você recuperou 25 de vida')
                         else:
-                            hp_mob1 = hp_mob1 - 20
-                            print('Ataque crítico! 20 de dano')
-                            print('O inimigo ataca de volta te dando {} de dano'.format(ataque_mob))
-                            hp0=hp0-ataque_mob
-                    elif decisao == 2:
-                        defesa = int(randint(1, 20))
-                        if defesa < 5:
-                            hp0 = hp0 - 5
-                            print('Sua defesa falhou')
-                            print('Você levou 5 de dano')
-                        else:
-                            print('Você bloqueou o ataque com sucesso!')
-                    elif decisao == 3:
-                        fuga = int(randint(1, 20))
-                        if fuga < 4:
-                            print('Parece que algúem não quer que você fuja')
-                        else:
-                            print('Você escapou com sucesso')
-                            fugiu=fugiu+1
-                            if fugiu==1:
-                                break
-                    elif decisao == 4:
-                        print('Qual item você deseja usar?')
-                        inv=int(input('Inventário:\n    1-Poção - {}    2-Voltar'.format(pot)))
-                        if inv==1:
-                            if pot>=1:
-                                hp0=hp0+25
-                                print('Você recuperou 25 de vida')
-                            else:
-                                print('Você não tem poções')
-                if fugiu==1:
-                    print('Você ganhou 100 pontos!')
-                    pts=pts+100
-                else:
-                    print('Você ganhou 200 pontos!')
-                    pts=pts+200
+                            print('Você não tem poções')
+            if fugiu==1:
+                print('Você ganhou 100 pontos!')
+                pts=pts+100
+            else:
+                print('Você ganhou 200 pontos!')
+                pts=pts+200
     if bif=='direita' or bif=='Direita':
         bau=int(randint(0,100))
         if bau<20:
@@ -91,55 +93,64 @@ while hp0 > 0:
         trap2=int(randint(0,100))
         if trap2<30:
             hp_mob2 = 35
-            print('Era o brilho de um monstro aquático!')
-            while hp_mob2 > 0:
-                print('Mob: {}  Você: {}'.format(hp_mob2, hp0))
-                decisao = int(input('1-Atacar   2-Defender    3-Fugir     4-Item: '))
-                if decisao == 1:
-                    combate = int(randint(1, 20))
-                    if combate < 10 and combate>4:
-                        hp_mob2 = hp_mob2 - 5
-                        print('Você deu 5 de dano')
-                    elif combate < 3:
-                        print('Errou!')
-                    elif combate > 10 and combate < 20:
-                        hp_mob2 = hp_mob2 - 10
-                        print('Você deu 10 de dano')
+        elif trap2>75:
+            hp_mob2=50
+        print('Era o brilho de um monstro aquático!')
+        while hp_mob2 > 0:
+            ataque_mob2=randint(1,15)
+            print('Mob: {}  Você: {}'.format(hp_mob2, hp0))
+            decisao = int(input('1-Atacar   2-Defender    3-Fugir     4-Item: '))
+            if decisao == 1:
+                combate = int(randint(1, 20))
+                if combate < 10 and combate>4:
+                    hp_mob2 = hp_mob2 - 5
+                    print('Você deu 5 de dano')
+                    print('O inimigo ataca de volta te dando {} de dano.'.format(ataque_mob2))
+                    hp0 = hp0 - ataque_mob2
+                elif combate < 3:
+                    print('Errou!')
+                elif combate > 10 and combate < 20:
+                    hp_mob2 = hp_mob2 - 10
+                    print('Você deu 10 de dano')
+                    print('O inimigo ataca de volta te dando {} de dano.'.format(ataque_mob2))
+                    hp0 = hp0 - ataque_mob2
+                else:
+                    hp_mob2 = hp_mob2 - 20
+                    print('Ataque crítico! 20 de dano')
+                    print('O inimigo ataca de volta te dando {} de dano.'.format(ataque_mob2))
+                    hp0 = hp0 - ataque_mob2
+            elif decisao == 2:
+                defesa = int(randint(1, 20))
+                if defesa < 5:
+                    hp1 = hp0 - 5
+                    print('Sua defesa falhou')
+                    print('Você levou 5 de dano')
+                else:
+                    print('Você bloqueou o ataque com sucesso!')
+            elif decisao == 3:
+                fuga = int(randint(1, 20))
+                if fuga < 4:
+                    print('Parece que algúem não quer que você fuja')
+                else:
+                    print('Você escapou com sucesso')
+                    fugiu = 1
+                    if fugiu == 1:
+                        break
+            elif decisao == 4:
+                print('Qual item você deseja usar?')
+                inv = int(input('Inventário:\n    1-Poção - {}    2-Voltar'.format(pot)))
+                if inv == 1:
+                    if pot >= 1:
+                        hp0 = hp0 + 25
+                        print('Você recuperou 25 de vida')
                     else:
-                        hp_mob2 = hp_mob2 - 20
-                        print('Ataque crítico! 20 de dano')
-                elif decisao == 2:
-                    defesa = int(randint(1, 20))
-                    if defesa < 5:
-                        hp1 = hp0 - 5
-                        print('Sua defesa falhou')
-                        print('Você levou 5 de dano')
-                    else:
-                        print('Você bloqueou o ataque com sucesso!')
-                elif decisao == 3:
-                    fuga = int(randint(1, 20))
-                    if fuga < 4:
-                        print('Parece que algúem não quer que você fuja')
-                    else:
-                        print('Você escapou com sucesso')
-                        fugiu = 1
-                        if fugiu == 1:
-                            break
-                elif decisao == 4:
-                    print('Qual item você deseja usar?')
-                    inv = int(input('Inventário:\n    1-Poção - {}    2-Voltar'.format(pot)))
-                    if inv == 1:
-                        if pot >= 1:
-                            hp0 = hp0 + 25
-                            print('Você recuperou 25 de vida')
-                        else:
-                            print('Você não tem poções')
-            if fugiu == 1:
-                print('Você ganhou 100 pontos!')
-                pts = pts + 100
-            else:
-                print('Você ganhou 200 pontos!')
-                pts = pts + 200
+                        print('Você não tem poções')
+        if fugiu == 1:
+            print('Você ganhou 100 pontos!')
+            pts = pts + 100
+        else:
+            print('Você ganhou 200 pontos!')
+            pts = pts + 200
 
 
 print('Você perdeu')
